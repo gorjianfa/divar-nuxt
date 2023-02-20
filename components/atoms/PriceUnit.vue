b337ed34fec149a329ac2b3215e618a240753bda
gorjianfa <grjyanfatmh3@gmail.com>
2023-02-20 17:00:52 +0330

    [ADD]: PriceUnit component

---
+++ components/atoms/PriceUnit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tw-bg-gray-50 tw-text-red-500">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
